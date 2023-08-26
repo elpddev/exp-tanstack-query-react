b3141b88a0baff4dabf1f7aa4f3f17a9cdcec8b7
Eyal Lapid <elpddev@pm.me>
2023-08-26 12:24:26 +0300

    fix lint

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // https://vitejs.dev/guide/static-deploy.html#github-pages 
-  base: "/exp-tanstack-query-react/"
+  // https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: "/exp-tanstack-query-react/",
 });
